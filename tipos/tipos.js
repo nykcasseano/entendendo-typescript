746b76e43c56344077773bb34e3c3bb2d14ef6e6
nykcasseano <nykcasseano13@gmail.com>
2023-09-07 09:21:55 -0300

    adding more files to the project

---
+++ tipos/tipos.js
@@ -0,0 +1,12 @@
+// string 
+var nome = ' joão';
+console.log(nome); // João
+// numbers
+var idade = 27;
+// idade = 'Ana'
+idade = 27.5;
+console.log(idade); //
+//boolean
+var possuiHobbies = false;
+//possuiHobbies = 1
+console.log(possuiHobbies);
---
+++ tipos/tipos.ts
@@ -0,0 +1,14 @@
+// string 
+let nome = ' joão'
+console.log(nome) // João
+
+// numbers
+let idade = 27 
+// idade = 'Ana'
+idade = 27.5
+console.log(idade) //
+
+//boolean
+let possuiHobbies = false
+//possuiHobbies = 1
+console.log(possuiHobbies) 
