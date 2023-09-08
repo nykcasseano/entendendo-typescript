564489c9ace3717ceb55b50fff4edd3558bb876e
nykcasseano <nykcasseano13@gmail.com>
2023-09-08 11:09:34 -0300

    adding more files to the project

---
+++ compilador/compilador.js
@@ -0,0 +1,5 @@
+var canal = 'Gaveta';
+var inscritos = 70000;
+//canal = inscritos
+console.log("Canal = ".concat(canal));
+// let nome= 'Pedro'
---
+++ compilador/compilador.ts
@@ -0,0 +1,5 @@
+let canal: string = 'Gaveta'
+let inscritos: number = 70000
+//canal = inscritos
+console.log(`Canal = ${canal}`)
+// let nome= 'Pedro'
