548c3e33ec0e37b0b0b5b8f0f6ed18bf8078ac67
nykcasseano <nykcasseano13@gmail.com>
2023-09-07 21:28:01 -0300

    adding more files to the project

--- tipos/tipos.js
+++ tipos/tipos.js
@@ -21,3 +21,24 @@ console.log(typeof hobbies);
 hobbies = [100, 200, 300];
 // hobbies =100
 console.log(hobbies);
+// tuplas
+var endereco = ["Av Principal", 99, "casa 2"];
+endereco = [" Rua Principal", 99, "casa 2"];
+// enums
+var Cor;
+(function (Cor) {
+    Cor[Cor["Cinza"] = 0] = "Cinza";
+    Cor[Cor["Verde"] = 100] = "Verde";
+    Cor[Cor["Azul"] = 10] = "Azul";
+    Cor[Cor["Laranja"] = 11] = "Laranja";
+    Cor[Cor["Amarelo"] = 12] = "Amarelo";
+    Cor[Cor["Vermelho"] = 100] = "Vermelho";
+})(Cor || (Cor = {}));
+var minhaCor = Cor.Cinza;
+console.log(minhaCor);
+console.log(Cor.Laranja);
+// any 
+var carro = 'BMW';
+console.log(carro);
+carro = { marca: 'BMW', ano: 2019 };
+console.log(carro);
