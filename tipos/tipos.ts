af3c60acdb81fc9490d04e179b52c856272e2c3d
nykcasseano <nykcasseano13@gmail.com>
2023-09-08 10:51:20 -0300

    adding more files to the project

--- tipos/tipos.ts
+++ tipos/tipos.ts
@@ -189,3 +189,31 @@ const produto = {
 }
 
 produto.validarProduto()
+
+let altura = 12
+// altura = null
+
+let alturaOpcional: null | number = 12
+alturaOpcional = null
+
+type Contato = {
+    nome: string,
+    tel1: string,
+    tel2: string | null
+}
+
+const contato1: Contato = {
+    nome: 'Fulano',
+    tel1: '98765432',
+    tel2: null
+}
+
+console.log(contato1.nome)
+console.log(contato1.tel1)
+console.log(contato1.tel2)
+
+let podeSerNulo = null
+podeSerNulo = 12
+console.log(podeSerNulo)
+podeSerNulo = 'abc'
+console.log(podeSerNulo)
