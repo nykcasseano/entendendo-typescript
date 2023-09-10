8987038595f8cb1f554ac451f10655223e48be6e
nykcasseano <nykcasseano13@gmail.com>
2023-09-10 20:28:52 -0300

    adding more files to the project

--- ecmascript/ecmascript.ts
+++ ecmascript/ecmascript.ts
@@ -1,3 +1,15 @@
 // let & Const
-console.log(seraaQuePode)
-var seraQuePode = '?' // hoisting
+
+let seraQuePode = '?' // hoisting
+console.log(seraQuePode)
+
+let estaFrio = true //
+if (estaFrio){
+    let acao = "Colocar o Casaco"
+    console.log(acao)
+}
+
+
+const cpf: string = '123.456.000-99'
+// cpf = string
+console.log(cpf)
