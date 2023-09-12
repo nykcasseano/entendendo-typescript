fc91cad08a0bd29a55d885ac2ba8648b7088c721
nykcasseano <nykcasseano13@gmail.com>
2023-09-11 21:52:11 -0300

    adding more files to the project

--- ecmascript/ecmascript.ts
+++ ecmascript/ecmascript.ts
@@ -30,3 +30,12 @@ console.log(segredo)
         console.log(def)
     }
 }
+
+// Arrow Function 
+function somar(n1: number, n2: number): number {
+    return n1 + n2
+}
+console.log(somar(2, 2)
+
+const subtrair = (n1: number, n2:number): number => n1 - n2
+console.log(subtrair(2, 3))
