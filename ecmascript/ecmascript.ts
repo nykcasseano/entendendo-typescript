3c1d5167be7c410578992a4ebbd4677372a3be3d
nykcasseano <nykcasseano13@gmail.com>
2023-09-11 21:41:58 -0300

    adding more files to the project

--- ecmascript/ecmascript.ts
+++ ecmascript/ecmascript.ts
@@ -13,3 +13,20 @@ if (estaFrio){
 const cpf: string = '123.456.000-99'
 // cpf = string
 console.log(cpf)
+
+
+var segredo = 'externo!'
+function revelar(){
+    const segredo = 'interno'
+    console.log(segredo)
+
+}
+revelar()
+console.log(segredo)
+
+{
+    {
+        const def ='def'
+        console.log(def)
+    }
+}
